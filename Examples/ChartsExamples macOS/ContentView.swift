f68b46a971da4e16ad296b1907ade675dc5cfe54
Alex <alex.buzov@icloud.com>
2019-11-26 19:06:01 -0800

    targets

---
+++ Examples/ChartsExamples macOS/ContentView.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            LineChartsView()
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Text("Line")
+                    }
+                }
+                .tag(0)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Charts",
+    platforms: [
+        .iOS(.v13), .macOS(.v10_15), .watchOS(.v6), .tvOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
