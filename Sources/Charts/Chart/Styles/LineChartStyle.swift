4475687784b857ad5bbc3d3e9bc608290a70630e
Alex <alex.buzov@icloud.com>
2019-11-27 00:30:11 -0800

    area chart style

--- Sources/Charts/Chart/Styles/LineChartStyle.swift
+++ Sources/Charts/Chart/Styles/LineChartStyle.swift
@@ -2,15 +2,28 @@ import SwiftUI
 import Shapes
 
 public struct LineChartStyle: ChartStyle {
+    public enum LineType {
+        case line
+        case quadCurve
+    }
+    
+    private let lineType: LineType
     private let lineColor: Color
     private let lineWidth: CGFloat
     
     public func makeBody(configuration: Self.Configuration) -> some View {
-        QuadCurve(unitData: configuration.data)
-            .stroke(self.lineColor, style: .init(lineWidth: self.lineWidth, lineCap: .round))
+        switch lineType {
+        case .line:
+            return AnyView(Line(unitData: configuration.data)
+                .stroke(self.lineColor, style: .init(lineWidth: self.lineWidth, lineCap: .round)))
+        case .quadCurve:
+            return AnyView(QuadCurve(unitData: configuration.data)
+                .stroke(self.lineColor, style: .init(lineWidth: self.lineWidth, lineCap: .round)))
+        }
     }
     
-    public init(lineColor: Color = .accentColor, lineWidth: CGFloat = 1) {
+    public init(_ lineType: LineType = .quadCurve, lineColor: Color = .accentColor, lineWidth: CGFloat = 1) {
+        self.lineType = lineType
         self.lineColor = lineColor
         self.lineWidth = lineWidth
     }
