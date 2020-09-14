7b8dc3c1ba692ef12812b6c1f96cacf9f715c000
Alex <alex.buzov@icloud.com>
2020-09-14 12:37:08 -0700

    playgrounds

--- Tests/ChartsTests/ChartsTests.swift
+++ Tests/ChartsTests/ChartsTests.swift
@@ -6,7 +6,7 @@ final class ChartsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Charts().text, "Hello, World!")
+        //XCTAssertEqual(Charts().text, "Hello, World!")
     }
 
     static var allTests = [
