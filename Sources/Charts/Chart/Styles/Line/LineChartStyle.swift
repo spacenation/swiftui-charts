a87d01c8041f90c6618541654ee2e563304f4830
Cole Dunsby <coledunsby@gmail.com>
2020-10-01 09:54:58 -0400

    Add `trim` params to LineChartStyle

--- Sources/Charts/Chart/Styles/Line/LineChartStyle.swift
+++ Sources/Charts/Chart/Styles/Line/LineChartStyle.swift
@@ -2,24 +2,33 @@ import SwiftUI
 import Shapes
 
 public struct LineChartStyle: ChartStyle {
+
     private let lineType: LineType
     private let lineColor: Color
     private let lineWidth: CGFloat
-    
-    public func makeBody(configuration: Self.Configuration) -> some View {
+
+    @Binding private var trimFrom: CGFloat
+    @Binding private var trimTo: CGFloat
+
+    @ViewBuilder
+    public func makeBody(configuration: Configuration) -> some View {
         switch lineType {
         case .line:
-            return AnyView(Line(unitData: configuration.dataMatrix.map { $0.reduce(0, +) })
-                .stroke(self.lineColor, style: .init(lineWidth: self.lineWidth, lineCap: .round)))
+            Line(unitData: configuration.dataMatrix.map { $0.reduce(0, +) })
+                .trim(from: trimFrom, to: trimTo)
+                .stroke(lineColor, style: .init(lineWidth: lineWidth, lineCap: .round))
         case .quadCurve:
-            return AnyView(QuadCurve(unitData: configuration.dataMatrix.map { $0.reduce(0, +) })
-                .stroke(self.lineColor, style: .init(lineWidth: self.lineWidth, lineCap: .round)))
+            QuadCurve(unitData: configuration.dataMatrix.map { $0.reduce(0, +) })
+                .trim(from: trimFrom, to: trimTo)
+                .stroke(lineColor, style: .init(lineWidth: lineWidth, lineCap: .round))
         }
     }
     
-    public init(_ lineType: LineType = .quadCurve, lineColor: Color = .accentColor, lineWidth: CGFloat = 1) {
+    public init(_ lineType: LineType = .quadCurve, lineColor: Color = .accentColor, lineWidth: CGFloat = 1, trimFrom: Binding<CGFloat> = .constant(0), trimTo: Binding<CGFloat> = .constant(1)) {
         self.lineType = lineType
         self.lineColor = lineColor
         self.lineWidth = lineWidth
+        _trimFrom = trimFrom
+        _trimTo = trimTo
     }
 }
